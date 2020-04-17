90464a270f29318f4894058120dc5d828db203a4
AmrMossad93 <eng.amrmossad93@gmail.com>
2020-04-18 00:22:02 +0200

    Card Wudgit

--- src/app/Modules/shared.module.ts
+++ src/app/Modules/shared.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CardComponent} from '../Widgets/card/card.component';
 import {IonicModule} from '@ionic/angular';
+import {CountUpModule} from 'ngx-countup';
 
 
 @NgModule({
     declarations: [CardComponent],
     imports: [
         CommonModule,
+        CountUpModule,
         IonicModule
     ],
     exports: [
--- src/app/Widgets/card/card.component.ts
+++ src/app/Widgets/card/card.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss'],
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
+    @Input() iconClass: string;
+    @Input() tittle: string;
+    @Input() color: string;
+    @Input() numberCases: number;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {}
+    ngOnInit() {
+    }
 
 }
