c221092db0ad0c97a0e68c11d7ffe08774db434f
AmrMossad93 <eng.amrmossad93@gmail.com>
2020-04-18 23:52:23 +0200

    Country Cases

--- src/app/Pages/reports/reports.module.ts
+++ src/app/Pages/reports/reports.module.ts
@@ -1,20 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { ReportsPageRoutingModule } from './reports-routing.module';
+import {ReportsPageRoutingModule} from './reports-routing.module';
 
-import { ReportsPage } from './reports.page';
+import {ReportsPage} from './reports.page';
+import {SharedModule} from '../../Modules/shared.module';
+import {PrimeNgModule} from '../../Modules/prime-ng.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    ReportsPageRoutingModule
-  ],
-  declarations: [ReportsPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        ReportsPageRoutingModule,
+        PrimeNgModule,
+        SharedModule
+    ],
+    declarations: [ReportsPage]
 })
-export class ReportsPageModule {}
+export class ReportsPageModule {
+}
--- src/app/Pages/reports/reports.page.ts
+++ src/app/Pages/reports/reports.page.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CoronaService} from '../../Services/corona.service';
+import {ICountry} from '../../Models/country';
+import {ICorona} from '../../Models/corona';
 
 @Component({
-  selector: 'app-reports',
-  templateUrl: './reports.page.html',
-  styleUrls: ['./reports.page.scss'],
+    selector: 'app-reports',
+    templateUrl: './reports.page.html',
+    styleUrls: ['./reports.page.scss'],
 })
 export class ReportsPage implements OnInit {
+    countries: ICountry;
+    myCountry;
+    countryList: string[] = [''];
+    data: ICorona;
+    chartData: any;
 
-  constructor() { }
+    constructor(private coronaService: CoronaService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.getCountries();
+    }
 
+    getCountries() {
+        this.coronaService.getCountries().subscribe(res => {
+            this.countries = res as ICountry;
+            this.countryList = this.countries.countries.map((c) => c.name);
+        });
+    }
+
+    getCoronaDetailsByCountry() {
+        this.coronaService.getCoronaDetailsByName(this.myCountry).subscribe(res => {
+            this.data = res as ICorona;
+        }, error => {
+        }, () => {
+            this.callChart();
+        });
+    }
+
+    callChart() {
+        this.chartData = {
+            labels: ['Infected', 'Recovered', 'Deaths'],
+            datasets: [
+                {
+                    data: [this.data.confirmed.value, this.data.recovered.value, this.data.deaths.value],
+                    backgroundColor: [
+                        '#17a2b8',
+                        '#28a745',
+                        '#dc3545'
+                    ],
+                    hoverBackgroundColor: [
+                        '#17a2b8',
+                        '#28a745',
+                        '#dc3545'
+                    ]
+                }]
+        };
+        return this.chartData;
+    }
 }
